3de9c24b1073377f335f651b2a5d2254ec3aec05
Jacob Gil <jacobgil325@gmail.com>
2022-03-16 15:39:32 -0400

    added back button on fragment holder as well to go back to launch screen if needed

--- app/src/main/java/edu/uga/cs/countrytovisitv2/FragmentHolderActivity.java
+++ app/src/main/java/edu/uga/cs/countrytovisitv2/FragmentHolderActivity.java
@@ -1,7 +1,9 @@
 package edu.uga.cs.countrytovisitv2;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 //This Activity simply holds the fragments -- fragments cannot exist alone
@@ -10,7 +12,23 @@ public class FragmentHolderActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.fragment_display);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null)
+            actionBar.setDisplayHomeAsUpEnabled( true );
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Implement Back button listener method.
+        int id = item.getItemId();
+
+        if( id == android.R.id.home ) {
+            onBackPressed();
+            return true;
+        }
+
+        return super.onOptionsItemSelected( item );
     }
 }
